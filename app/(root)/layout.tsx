060cff107c0bbed904a4967f9d1e567428bec8dd
SiddheshAR <siddheshfr@gmail.com>
2025-07-15 02:47:39 +0530

    setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({children}:Readonly<{children:React.ReactNode}>){
+
+    return(
+        <main className="text-2xl">
+            {children}
+        </main>
+    )
+}
